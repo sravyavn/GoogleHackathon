50d74add57eb6535030334d0d7e3551bad180ddd
Sravya Vemuri <sravyavn96@gmail.com>
2025-04-23 00:17:52 -0400

    app.py

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ from tavily import TavilyClient
 dotenv.load_dotenv()
 api_key = os.getenv("API_KEY")
 genai.configure(api_key=api_key)
-tavily_api_key = os.getenv("TAVILY_API_KEY")
-tavily = TavilyClient(api_key=tavily_api_key)
+tavily_api_key = st.secrets["TAVILY_API_KEY"]
 model = genai.GenerativeModel("gemini-2.0-flash-exp-image-generation")
 
 st.set_page_config(page_title="Safety Detector", page_icon="🕵️‍♀️", layout="centered")
